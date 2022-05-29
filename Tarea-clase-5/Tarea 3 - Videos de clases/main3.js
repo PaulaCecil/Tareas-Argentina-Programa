34d3c08560b1dc670c5ce3c5ed3eb3935779fbdb
PaulaCecil <paulaburruni@gmail.com>
2022-05-29 17:05:54 -0300

    tarea 2 terminada

---
+++ Tarea-clase-5/Tarea 3 - Videos de clases/main3.js
@@ -0,0 +1,100 @@
+
+const Horas = 0
+
+document.querySelector('#ingresar').onclick=function(){
+
+    const Horas = document.querySelectorAll('li');
+    //const Minutos = document.querySelectorAll('.listaMinutos').value;
+    //const Segundos = document.querySelectorAll('.listSsegundos').value;
+
+         
+    
+}
+
+/*function sumaDelTiempoTotal(Horas, Minutos, Segundos,){
+
+    return Horas+Minutos+Segundos;
+
+}
+
+
+document.querySelector('#calcular-tiempo-total').onclick=function(){
+
+    
+    console.log(`la suma del tiempo total de clases es ${sumaDelTiempoTotal}`);
+
+}
+
+
+*/
+
+
+
+
+
+
+
+
+/*document.querySelector('#ingresar').onclick=function(){
+
+    const Horas = Number (document.querySelector('#horas').value);
+    const Minutos = Number (document.querySelector('#minutos').value);
+    const Segundos = Number (document.querySelector('#segundos').value);
+
+    let HorasTotal = [0];
+    let MinutosTotal = [];
+    let SegundosTotal = [];
+
+    let sumaHorasTotales = [];
+
+    let tiempoTotal = [];
+
+    HorasTotal.push (Horas);
+
+   
+    for (let m=0; m<HorasTotal.length; m++){
+
+        sumaHorasTotales= sumaHorasTotales + HorasTotal[m];
+
+    }
+
+    function sumaDelTiempoTotal(sumaHorasTotales, tiempoTotal){
+
+        return tiempoTotal = sumaHorasTotales;
+
+    }
+
+    document.querySelector('#calcular-tiempo-total').onclick=function(){
+
+        
+        console.log(`la suma del tiempo total de clases es ${sumaDelTiempoTotal}`);
+
+    }
+
+}*/
+
+
+
+/*let numerosImpares=[];
+let totalSumaNumerosImpares=0;
+
+
+for (let i= 10; i<30; i++){
+
+    resultado=i;
+    
+    if (resultado%2==!0){
+
+        numerosImpares.push (resultado);
+    }
+
+}
+
+for (let m=0; m<numerosImpares.length; m++){
+
+    totalSumaNumerosImpares= totalSumaNumerosImpares + numerosImpares[m];
+
+    
+}
+
+console.log(totalSumaNumerosImpares);*/
